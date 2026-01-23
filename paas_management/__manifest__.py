4073d024223c33ef080675154659c7ba8df6f0ab
Lucas <llabeau@aureofy.com>
2026-01-23 02:36:55 -0300

    v0.0.9

--- paas_management/__manifest__.py
+++ paas_management/__manifest__.py
@@ -14,6 +14,7 @@
     'data': [
         'security/ir.model.access.csv',
         'data/res_partner_data.xml',
+        'views/paas_instance_views.xml',
     ],
     'installable': True,
     'application': True,
