f207a4e645126a65ad94528834c817dfc0bfe400
aureofy-desarrollo <llabeau@aureofy.com>
2026-02-13 15:52:06 -0300

    Trial Commit

--- paas_management/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from . import paas_config
-from . import paas_client
-from . import paas_sync
-from . import paas_user
-from . import paas_instance
-from . import paas_plan
-from . import res_partner
